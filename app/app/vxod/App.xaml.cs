7a2b91fc92dfc51abfbab91dce5921a3201e8b7c
Никита Никитов <n.damnation@gmail.com>
2023-04-01 20:40:00 +0300

    папки

--- app/app/vxod/App.xaml.cs
+++ app/app/vxod/App.xaml.cs
@@ -8,16 +8,10 @@ using System.Windows;
 
 namespace app
 {
-    
-
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для App.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class App : Application
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
     }
 }
---
+++ app/app/vxod/AuthorizationWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace app
+{
+    
+
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
