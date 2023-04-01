f425ba1bab0e5818f6be0820232ef7f6eb495213
Никита Никитов <n.damnation@gmail.com>
2023-04-01 17:16:03 +0300

    !

--- app/app/Week.cs
+++ app/app/Week.cs
@@ -1,4 +1,11 @@
-﻿namespace app
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+namespace app
 {
     internal class Week
     {
