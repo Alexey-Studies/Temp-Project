ed58e3814745a76826343531fc2963b091fd3481
daniil vitvicki <vdaniil08041971@gmail.com>
2023-04-03 14:11:14 +0300

    переход

--- app/app/AuthorizationWindow.xaml.cs
+++ app/app/AuthorizationWindow.xaml.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 
 
 namespace app
 {
-    
+
 
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
@@ -20,5 +14,13 @@ namespace app
         {
             InitializeComponent();
         }
+
+        private void entrance_window(object sender, RoutedEventArgs e)
+        {
+            Entrance entrance = new Entrance();
+            entrance.Show();
+            Hide();
+        }
+
     }
 }
---
+++ app/app/Entrance.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace app
+{
+    /// <summary>
+    /// Логика взаимодействия для entrance.xaml
+    /// </summary>
+    public partial class Entrance : Window
+    {
+        public Entrance()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_enter_Click(object sender, RoutedEventArgs e)
+        {
+            Calendar calendar = new Calendar();
+            calendar.Show();
+            Hide();
+        }
+    }
+}
