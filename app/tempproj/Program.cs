2c36a21dc731a97ab4f54e6ebcf5e761fa39e240
kimkrmadjan@gmail.com <kimkrmadjan@gmail.com>
2023-04-04 20:32:47 +0300

    !

--- app/tempproj/Program.cs
+++ app/tempproj/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 namespace app
 {
     class kakoito
     {
         public static void Main()
         {
-            string nameOF = "Математика";
-           Day day = new Day();
-            day.GethoursLessons(nameOF);
+            // string nameOF = "Математика";
+            //Day day = new Day();
+            // day.GethoursLessons(nameOF);
+            Week week = new Week();
+            week.Predmet();
         }
+
     }
 }
--- app/tempproj/coretemp/Week.cs
+++ app/tempproj/coretemp/Week.cs
@@ -10,62 +10,24 @@ namespace app
     class Week
     {
         public bool typeweek { get; set; }
-        struct Monday
-        {
-            public Lesson para1;
-            public Lesson para2;
-            public Lesson para3;
-            public Lesson para4;
-            public Lesson para5;
-        }
-        struct Tuesday
-        {
-            public Lesson para1;
-            public Lesson para2;
-            public Lesson para3;
-            public Lesson para4;
-            public Lesson para5;
 
-        }
-        struct Wednesday
+        public void KI()
         {
-            public Lesson para1;
-            public Lesson para2;
-            public Lesson para3;
-            public Lesson para4;
-            public Lesson para5;
+            List<string> Monday = new List<string>();
+            Monday.Add("Моделирование цифровых узлов компьютерных систем");
+            Monday.Add("-");
+            Monday.Add("Численные методы и операции исчисления");
+            Monday.Add("Моделирование цифровых узлов компьютерных систем");
+            Monday.Add("-");
         }
-        struct Thursday
+        public void Predmet()
         {
-            public Lesson para1;
-            public Lesson para2;
-            public Lesson para3;
-            public Lesson para4;
-            public Lesson para5;
-        }
-        struct Friday
-        {
-            public Lesson para1;
-            public Lesson para2;
-            public Lesson para3;
-            public Lesson para4;
-            public Lesson para5;
-        }
-        struct Saturday
-        {
-            public Lesson para1;
-            public Lesson para2;
-            public Lesson para3;
-            public Lesson para4;
-            public Lesson para5;
-        }
-        struct Sunday
-        {
-            public Lesson para1;
-            public Lesson para2;
-            public Lesson para3;
-            public Lesson para4;
-            public Lesson para5;
+            Vector<Group> disc = new Vector<Group>();
+            Pofig.Add(Monday);
+            foreach (var ex in disc)
+            {
+                Console.Writeline(ex);
+            }
         }
 
     }
