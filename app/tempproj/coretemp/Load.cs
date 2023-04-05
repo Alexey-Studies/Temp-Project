6d12bbd2ecea259a0804665030635760a65312f9
daniil vitvicki <vdaniil08041971@gmail.com>
2023-04-05 14:24:29 +0300

    !

--- app/tempproj/coretemp/Load.cs
+++ app/tempproj/coretemp/Load.cs
@@ -1,40 +1,45 @@
-﻿//namespace tempproj.coretemp
-//{
-//    internal class Load
-//    {
-//        enum TypesOfClasses //ВИДЫ ЗАНЯТИЙ															
+﻿namespace tempproj.coretemp
+{
+    internal class Load : SubjectData
+    {
 
-//        public void Processing(TypesOfClasses type)  //обработка
-//        {
-//            switch (type)
-//            {
-//                case 0:
+        enum TypesOfClasses //ВИДЫ ЗАНЯТИЙ															
+        {
+            Lectures = 0,
+            PracticalExercises = 1,
+            LaboratoryWork = 2,
+            Seminars = 3,
+            CourseConsultations = 4,
+            ManagementOfDiplomaDesign = 5,
+            CourseProjectWork = 6,
+            Credits = 7,
+            Exams = 8,
+            ManagementOfGraduateStudents = 9,
+            ManagementOfProductionPractice = 10,
+            ManagementOfEducationalPractice = 11,
+            CheckingTheWorOfPartTimeStudents = 12,
+            GEK = 13,
+            MRK = 14,
+            Total = 15
+        }
+        public void Processing(int type)  //обработка
+        {
+            switch (type)
+            {
+                case 0:
 
-//                default:
-//                    break;
-//            }
-//        }
+                    break;
+                case 15:
+                //я не понимаю как работает это
 
-//        enum TypesOfClasses //ВИДЫ ЗАНЯТИЙ															
-//        {
-//            Lectures = 0,
-//            PracticalExercises = 1,
-//            LaboratoryWork = 2,
-//            Seminars = 3,
-//            CourseConsultations = 4,
-//            ManagementOfDiplomaDesign = 5,
-//            CourseProjectWork = 6,
-//            Credits = 7,
-//            Exams = 8,
-//            ManagementOfGraduateStudents = 9,
-//            ManagementOfProductionPractice = 10,
-//            ManagementOfEducationalPractice = 11,
-//            CheckingTheWorOfPartTimeStudents = 12,
-//            GEK = 13,
-//            MRK = 14,
-//            Total = 15
-//        }
+                default:
+                    break;
 
-//    }
-//}
+            }
+        }
+
+
+
+    }
+}
 
--- app/tempproj/coretemp/SubjectData.cs
+++ app/tempproj/coretemp/SubjectData.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace tempproj.coretemp
+﻿namespace tempproj.coretemp
 {
     internal class SubjectData
     {
+        struct EnglishLanguage
+        {
+            public string Name = "Английский";
+            public string[] GroupsThatHaveAClass = { "KI-21", "PI-21", "KSC-21" };
+            enum type
+            {
+
+            }
+            public EnglishLanguage()
+            {
+            }
+        }
     }
 }
